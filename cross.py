7fd915ed1a847e61e5858608a2aa2f9b590c6e71
Li SH <li012589li@outlook.com>
2018-10-23 01:43:43 +0800

    udpate

--- cross.py
+++ cross.py
@@ -5,9 +5,9 @@ import argparse
 
 parser = argparse.ArgumentParser(description="")
 
-parser.add_argument("-J",type = float,defautl=1,help="")
-parser.add_argument("-T",type = float,defautl=1,help="")
-parser.add_argument("-iters",type = int,defautl=20,help="")
+parser.add_argument("-J",type = float,default=1,help="")
+parser.add_argument("-T",type = float,default=1,help="")
+parser.add_argument("-iters",type = int,default=20,help="")
 parser.add_argument("-cut", type = int, default=20,help="")
 
 args = parser.parse_args()
@@ -26,7 +26,8 @@ T  = torch.einsum('ai,aj,ka,la->ijkl',(P,P,PT,PT))
 
 lnZ = torch.zeros(1)
 
-cut = args.cut
+maxCut = args.cut
+D = 2
 epsilon=0
 
 for n in range(args.iters):
@@ -34,18 +35,33 @@ for n in range(args.iters):
     T = T/maxT
     lnZ += 2**(args.iters-n)*torch.log(maxT)
 
-    TypeA = T.view(2**2,2**2)
+    TypeA = T.view(D**2,D**2)
 
-    TypeB = T.permute(0,3,1,2).contiguous().view(2**2,2**2)
+    TypeB = T.permute(0,3,1,2).contiguous().view(D**2,D**2)
 
     Ua,Sa,Va = torch.svd(TypeA)
     Ub,Sb,Vb = torch.svd(TypeB)
 
-    D = min(min(2**2,cut),min((Sa>epsilon).sum().item(),(Sb>epsilon).sum().item()))
+    cut = min(maxCut,min((Sa>epsilon).sum().item(),(Sb>epsilon).sum().item()))
+    print(cut)
+    print(maxT)
 
-    S2 = torch.matmul(Ub,torch.diag(torch.sqrt(Sb))).view(2,2,4)
-    S1 = torch.matmul(torch.diag(torch.sqrt(Sb)),Vb.t()).view(4,2,2)
-    S3 = torch.matmul(Ua,torch.diag(torch.sqrt(Sa))).view(2,2,4)
-    S4 = torch.matmul(torch.diag(torch.sqrt(Sa)),Va.t()).view(4,2,2)
+    S2 = torch.matmul(Ub[:,:cut],torch.diag(torch.sqrt(Sb[:cut]))).view(D,D,cut)
+    S1 = torch.matmul(torch.diag(torch.sqrt(Sb[:cut])),Vb.t()[:cut,:]).view(cut,D,D)
+    S3 = torch.matmul(Ua[:,:cut],torch.diag(torch.sqrt(Sa[:cut]))).view(D,D,cut)
+    S4 = torch.matmul(torch.diag(torch.sqrt(Sa[:cut])),Va.t()[:cut,:]).view(cut,D,D)
 
     T = torch.einsum('abc,bde,dfg,ncf->aegn',(S1,S3,S2,S4))
+    D = cut
+    #import pdb
+    #pdb.set_trace()
+
+trace = 0
+for i in range(D):
+    trace += T[i,i,i,i]
+    print(trace)
+print(lnZ)
+lnZ += torch.log(trace)
+
+print("K:",K.item())
+print("lnZ:",lnZ.item()/2**args.iters)
