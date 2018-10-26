3816d317265c49ad5278165ec162aa224b62f49e
Li SH <li012589li@outlook.com>
2018-10-27 01:02:15 +0800

    udpate

--- skewcorss.jl
+++ skewcorss.jl
@@ -10,19 +10,11 @@ function main()
 
     T = zeros(Float64,2,2,2,2)
 
-    no2s(no::Int64) = -2*no+3 ::Int64
-    sumS(s0::Int64,s1::Int64,s2::Int64,s3::Int64) = Float64(no2s(s0)*no2s(s1)+no2s(s1)*no2s(s2)+no2s(s2)*no2s(s3)+no2s(s3)*no2s(s0))
+    no2s(no::Int64) = -2*no+3
+    sumS(s0::Int64,s1::Int64,s2::Int64,s3::Int64) = no2s(s0)*no2s(s1)+no2s(s1)*no2s(s2)+no2s(s2)*no2s(s3)+no2s(s3)*no2s(s0)
 
-    @inbounds begin
-    for s0 = 1:2
-        for s1 = 1:2
-            for s2 = 1:2
-                for s3 = 1:2
-                    T[s3,s2,s1,s0] = exp(K*sumS(s3,s2,s1,s0))
-                end
-            end
-        end
-    end
+    for s0 = 1:2,s1 = 1:2,s2 = 1:2,s3 = 1:2
+        @inbounds T[s3,s2,s1,s0] = exp(K*sumS(s3,s2,s1,s0))
     end
 
     D = 2
@@ -40,11 +32,11 @@ function main()
         TypeB = reshape(permutedims(T,[1 4 2 3]),(D^2,D^2))
         ua,sa,va = svd(TypeA)
         ub,sb,vb = svd(TypeB)
-        cut = min(size(filter(checkBigger,sb))[1],size(filter(checkBigger,sa))[1],maxD)
-        S2 = reshape(ub[:,1:cut]*Diagonal(sqrt.(sb[1:cut])),(D,D,cut))
-        S1 = reshape(Diagonal(sqrt.(sb[1:cut]))*transpose(vb)[1:cut,:],(cut,D,D))
-        S3 = reshape(ua[:,1:cut]*Diagonal(sqrt.(sa[1:cut])),(D,D,cut))
-        S4 = reshape(Diagonal(sqrt.(sa[1:cut]))*transpose(va)[1:cut,:],(cut,D,D))
+        cut = min(count(checkBigger,sb),count(checkBigger,sa),maxD)
+        S2 = reshape(view(ub,:,1:cut)*Diagonal(sqrt.(view(sb,1:cut))),(D,D,cut))
+        S1 = reshape(Diagonal(sqrt.(view(sb,1:cut)))*view(transpose(vb),1:cut,:),(cut,D,D))
+        S3 = reshape(view(ua,:,1:cut)*Diagonal(sqrt.(view(sa,1:cut))),(D,D,cut))
+        S4 = reshape(Diagonal(sqrt.(view(sa,1:cut)))*view(transpose(va),1:cut,:),(cut,D,D))
 
         @tensor T[a,e,g,n] := S1[a,b,c]*S3[c,d,e]*S2[f,d,g]*S4[n,f,b]
         #T = Tp
