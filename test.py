d9ccb85388ccc65f1681be5465eea6d5d2ac7b60
Li S.H <li012589li@outlook.com>
2018-10-30 10:41:46 +0800

    add std

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import time
 import subprocess
+import numpy as np
 
 trys = 10
 command0 = ["python","./pytorch/trg.py"]
@@ -7,34 +8,34 @@ command1 = ["julia","./julia/trg.jl"]
 command2 = ["./itensor/trg"]
 
 def testrun(command,trys):
-    during = 0
+    during = []
     for _ in range(trys):
         start = time.time()
         subprocess.check_call(command)
         end = time.time()
-        during += (end - start)
-
-    return during/trys
+        during.append((end - start))
+    during = np.array(during)
+    return during.mean(),during.std()
 
 print("Testing pytorch")
 
-average0 = testrun(command0,trys)
+average0,std0 = testrun(command0,trys)
 
-print("pytorch average:",average0,"of",trys,"runs")
+print("pytorch average:",average0,"+/-",std0,"of",trys,"runs")
 
 print("Testing itensor")
 
-average2 = testrun(command2,trys)
+average2,std2 = testrun(command2,trys)
 
-print("itensor average:",average2,"of",trys,"runs")
+print("itensor average:",average2,"+/-",std2,"of",trys,"runs")
 
 print("Testing julia")
 
-average1 = testrun(command1,trys)
+average1,std1 = testrun(command1,trys)
 
-print("julia average:",average1,"of",trys,"runs")
+print("julia average:",average1,"+/-",std1,"of",trys,"runs")
 
-print("Final result: pytorch:",average0,"itensor:",average2,"julia:",average1)
+print("Final result: pytorch:",average0,"+/-",std0,"itensor:",average2,"+/-",std2,"julia:",average1,"+/-",std1)
 
 
 
