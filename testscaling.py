f8a274696d8fd8bb2948b8fbe468647d6b703d39
Li S.H <li012589li@outlook.com>
2018-10-30 10:42:19 +0800

    change maximumcut plot to log plot

--- testscaling.py
+++ testscaling.py
@@ -1,5 +1,6 @@
 import time
 import subprocess
+import numpy as np
 
 command0 = ["python","./pytorch/trg.py"]
 command1 = ["julia","./julia/trg.jl"]
@@ -7,64 +8,71 @@ command2 = ["./itensor/trg"]
 
 iterPoints = [10,20,30,40,50,60]
 cutPoints = [10,20,30,40,50]
-trys = 3
+trys = 2
 
 def testrun(command,trys):
-    during = 0
+    during = []
     for _ in range(trys):
         start = time.time()
         subprocess.check_call(command)
         end = time.time()
-        during += (end - start)
+        during.append((end - start))
+    during = np.array(during)
+    return during.mean(),during.std()
 
-    return during/trys
 
 def testdraw(command,trys,points,subfix = [],prefix = []):
     times = []
+    stds = []
     for ptr in points:
-        times.append(testrun(command+prefix+[str(ptr)]+subfix,trys))
-    return times
+        time,std = testrun(command+prefix+[str(ptr)]+subfix,trys)
+        times.append(time)
+        stds.append(std)
+    return np.array(times),np.array(stds)
 
 print("Testing pytorch scaling with iterations")
 
-times00 = testdraw(command0,trys,iterPoints,prefix = ["-iters"])
+times00,std00 = testdraw(command0,trys,iterPoints,prefix = ["-iters"])
 
 print("Testing pytorch scaling with maximum cut")
 
-times01 = testdraw(command0,trys,cutPoints,prefix = ["-cut"])
+times01,std01 = testdraw(command0,trys,cutPoints,prefix = ["-cut"])
 
 print("Testing itensor scaling with iterations")
 
-times20 = testdraw(command2,trys,iterPoints,prefix = ["1.0"],subfix=["20"])
+times20,std20 = testdraw(command2,trys,iterPoints,prefix = ["1.0"],subfix=["20"])
 
 print("Testing itensor scaling with maximum cut")
 
-times21 = testdraw(command2,trys,cutPoints,prefix = ["1.0","20"])
+times21,std21 = testdraw(command2,trys,cutPoints,prefix = ["1.0","20"])
 
 print("Testing julia scaling with iterations")
 
-times10 = testdraw(command1,trys,iterPoints,prefix = ["1.0"],subfix=["20"])
+times10,std10 = testdraw(command1,trys,iterPoints,prefix = ["1.0"],subfix=["20"])
 
 print("Testing julia scaling with maximum cut")
 
-times11 = testdraw(command1,trys,cutPoints,prefix = ["1.0","20"])
+times11,std11 = testdraw(command1,trys,cutPoints,prefix = ["1.0","20"])
 
 import matplotlib.pyplot as plt
 
-plt.figure()
-plt.plot(iterPoints,times00,label="pytorch")
-plt.plot(iterPoints,times20,label="itensor")
-plt.plot(iterPoints,times10,label="julia")
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.errorbar(iterPoints,times00,yerr=std00,label="pytorch")
+ax.errorbar(iterPoints,times20,yerr=std20,label="itensor")
+ax.errorbar(iterPoints,times10,yerr=std10,label="julia")
 
 plt.legend()
 plt.xlabel("Iterations")
 plt.ylabel("Times(s)")
 plt.title("Time Scaling with Iterations")
 
-plt.figure()
-plt.plot(cutPoints,times01,label="pytorch")
-plt.plot(cutPoints,times21,label="itensor")
-plt.plot(cutPoints,times11,label="julia")
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.errorbar(cutPoints,times01,yerr=std01,label="pytorch")
+ax.errorbar(cutPoints,times21,yerr=std21,label="itensor")
+ax.errorbar(cutPoints,times11,yerr=std11,label="julia")
+ax.set_yscale("log")
 
 plt.legend()
 plt.xlabel("Maximum Cut")
